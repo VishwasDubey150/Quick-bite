8b60c35f3a07a9ec7e888773b58cbe25867acbd0
Vishwas <VishwasDubey150@gmail.com>
2022-04-28 00:59:11 +0530

    first commit

---
+++ app/src/main/java/com/example/quick_byte/splash.java
@@ -0,0 +1,36 @@
+package com.example.quick_byte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.window.SplashScreen;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Thread thread=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent=new Intent(splash.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };thread.start();
+    }
+}
