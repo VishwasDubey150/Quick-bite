bb5a18f765c222d100122e98350230452fed7a2f
Vishwas <VishwasDubey150@gmail.com>
2022-05-15 01:14:04 +0530

    fragments installed

--- app/src/main/java/com/example/quick_byte/MainActivity.java
+++ app/src/main/java/com/example/quick_byte/MainActivity.java
@@ -1,15 +1,56 @@
 package com.example.quick_byte;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.container,new fhome1());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.cart:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.container,new fcart2());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.favourite:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.container,new ffavourite3());
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.myaccount:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.container,new faccount4());
+                        fragmentTransaction.commit();
+                        break;
+                    }
+                return true;
+            }
+        });
+
     }
 }
